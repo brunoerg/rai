a196571243e91510e9848afc901414c43b2e5688
bruno <bgarcia@3xbit.com.br>
2019-07-17 10:25:25 -0300

    fixing - flake8

--- rai/rai.py
+++ rai/rai.py
@@ -1,19 +1,15 @@
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import x25519
-from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from py_essentials import hashing as hs
-from nanolib import generate_seed
-from nanolib import generate_account_private_key
-from nanolib import Block
 import ed25519_blake2b
 from utils import decoder, encodWalletMensg
-import binascii
+import hashlib
+
 
 def generate_shared_secret(priv_key, address):
     return priv_key.exchange(load_public_key_from_bytes_x25519(address_to_publickey_bytes(address)))
+
 
 def save_message_in_address(message):
     message_len = len(message.encode('utf-8'))
@@ -33,6 +29,7 @@ def save_message_in_address(message):
             message_encoded.append(encodWalletMensg(m))
     return message_encoded
 
+
 def address_to_publickey_bytes(address):
     if address.startswith('xrb_'):
         address = address[4:]
@@ -48,18 +45,21 @@ def address_to_publickey_bytes(address):
         del address[i]
     return bytes(address)
 
-# sha1, sha256, sha512, md5
+
 def file_to_address(file_path, algorithm):
     hsh = hs.fileChecksum(file_path, algorithm)
     return save_message_in_address(hsh)
 
+
 def load_private_key_from_bytes(priv_bytes):
     return x25519.X25519PrivateKey.from_private_bytes(priv_bytes)
+
 
 def generate_x25519_private_public_key():
     priv_key = X25519PrivateKey.generate()
     pub_key = priv_key.public_key()
     return priv_key, pub_key
+
 
 def get_bytes_from_privkey_object(priv_key):
     return priv_key.priv_bytes(
@@ -68,19 +68,29 @@ def get_bytes_from_privkey_object(priv_key):
         encryption_algorithm=serialization.NoEncryption()
     )
 
+
 def load_public_key_from_bytes_x25519(pub_bytes):
     return x25519.X25519PublicKey.from_public_bytes(pub_bytes)
-    
+
+
 def load_public_key_bytes_from_addr_x25519(address):
     return x25519.X25519PublicKey.from_public_bytes(address_to_publickey_bytes(address))
+
 
 def get_pair_ed_key_from_seed(seed):
     sk = ed25519_blake2b.SigningKey(seed.encode('utf-8'))
     return sk, sk.get_verifying_key()
 
+
 def sign_message_from_seed(seed, message):
     sk = get_pair_ed_key_from_seed(seed)
     return sign_message(sk[0], message)
 
+
 def sign_message(signing_key, message):
     return signing_key.sign(message.encode('utf-8'), encoding="base64")
+
+
+def address_to_pubkey_blake_hash(address):
+    pub_key = address_to_publickey_bytes(address)
+    return hashlib.blake2b(pub_key).hexdigest()
--- rai/utils.py
+++ rai/utils.py
@@ -6,15 +6,19 @@ CUSTOM_ALPHABET = '13456789abcdefghijkmnopqrstuwxyz'
 ENCODE_TRANS = str.maketrans(STANDARD_ALPHABET, CUSTOM_ALPHABET)
 DECODE_TRANS = str.maketrans(CUSTOM_ALPHABET, STANDARD_ALPHABET)
 
+
 def encoder(input):
   return base64.b32encode(bytearray(input, 'utf-8')).decode('utf-8').translate(ENCODE_TRANS)
 
+
 def encodecheck(input):
-    w=bytearray.fromhex(input)
+    w = bytearray.fromhex(input)
     return base64.b32encode(w).decode('utf-8').translate(ENCODE_TRANS)
+
 
 def decoder(input):
   return base64.b32decode(input.translate(DECODE_TRANS))
+
 
 def encodWalletMensg(mens):
     mensExp = '   ' + str(mens)
@@ -26,6 +30,7 @@ def encodWalletMensg(mens):
     invchek = "".join(reversed([checksum[i:i+2] for i in range(0, len(checksum), 2)]))
     chekInvCod = encodecheck(invchek)
     return 'xrb_' + mensExpCod + chekInvCod
+
 
 def decodWalletMensg(wallet):
     mensCod = 'dxho' + wallet[4:-8]
